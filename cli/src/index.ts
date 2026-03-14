f99549da8c54da74bee75c7e4a16f313ab4e3e66
Akash Dayanand <akashdh11@gmail.com>
2026-03-13 23:58:05 -0400

    enhancement

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -15,7 +15,7 @@ const program = new Command();
 program
   .name('skystream')
   .description('SkyStream Plugin Development Kit CLI (Sky Gen 2)')
-  .version('1.4.8');
+  .version('1.4.9');
 
 // Schemas
 const pluginSchema = z.object({
